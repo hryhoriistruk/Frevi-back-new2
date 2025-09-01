8fd08075d7d736fcd8e02c6e5f55bef57e7bd24f
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2025-09-01 17:16:15 +0200

    Delete messaging-service directory

--- messaging-service/src/main/java/com/frevi/messaging/dto/request/KeyBundle.java
+++
@@ -1,9 +0,0 @@
-package com.frevi.messaging.dto.request;
-
-import java.util.List;
-
-public class KeyBundle {
-    public String userId;
-    public int deviceId = 0;
-    public List<String> preKeysBase64;
-}
--- messaging-service/src/main/java/com/frevi/messaging/dto/request/MessageRequest.java
+++
@@ -1,8 +0,0 @@
-package com.frevi.messaging.dto.request;
-
-public record MessageRequest(String fromUser,
-                             String toUser,
-                             String ephemeralPubBase64,
-                             String ciphertextBase64,
-                             String nonceBase64) {
-}
